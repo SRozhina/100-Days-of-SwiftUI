712b349adc270db90c549d9ec4dcafd80e0d0ff3
Sofia <rsofico@gmail.com>
2019-10-10 19:33:27 +0300

    Initial commit

---
+++ 1. WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Софья Рожина on 09.10.2019.
+//  Copyright © 2019 Софья Рожина. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    private var tipPercentages = [10, 15, 20, 25, 0]
+    
+    private var totalPerPerson: Double {
+        guard let peopleCount = Double(numberOfPeople) else {
+            return 0
+        }
+        let amountPerPerson = totalAmount / peopleCount
+
+        return amountPerPerson
+    }
+    
+    private var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        return grandTotal
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tips percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Total amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
